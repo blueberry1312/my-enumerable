91f3d88ccebd02d5b8637f8afb803b4c4fa37c70
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-05-11 18:50:46 -0400

    Add methods

--- MyListClass.rb
+++ MyListClass.rb
@@ -1,17 +1,37 @@
 require './MyEnumerable'
 
 class MyList
-  extend MyEnumerable
+  include MyEnumerable
 
   def initialize(*arg)
     @list = arg
   end
 
-  def each
+  def each(&block)
     return to_enum unless block_given?
 
-    for x in @list do
-      yield(x)
-    end
+    @list.each(&block)
   end
 end
+
+# Create new List
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test methods
+
+all_true_numbers = list.all? { |e| e < 5 }
+all_false_numbers = list.all? { |e| e > 5 }
+any_true_two = list.any? { |e| e == 2 }
+any_false_five = list.any? { |e| e == 5 }
+even_numbers = list.filter(&:even?)
+
+# Output Tests
+
+puts <<~TEXT
+  All < 5: #{all_true_numbers}
+  All > 5: #{all_false_numbers}
+  Any 2: #{any_true_two}
+  Any 5: #{any_false_five}
+  Even numbers found: #{even_numbers}
+TEXT
