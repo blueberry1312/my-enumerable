15ee8e4dc3abd18b63d3856d611ad625ddc9d61c
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-05-11 19:01:51 -0400

    fix linters

--- my_list_class.rb
+++ my_list_class.rb
@@ -1,18 +1,16 @@
 require './my_enumerable'
 
 class MyList
-include MyEnumerable
+  include MyEnumerable
 
   def initialize(*arg)
     @list = arg
   end
 
-  def each
+  def each(&block)
     return to_enum unless block_given?
 
-    for x in @list do
-      yield(x)
-    end
+    @list.each(&block)
   end
 end
 
@@ -22,11 +20,11 @@ list = MyList.new(1, 2, 3, 4)
 
 # Test methods
 
-all_true_numbers = list.all? {|e| e < 5}
-all_false_numbers = list.all? {|e| e > 5}
-any_true_two = list.any? {|e| e == 2}
-any_false_five = list.any? {|e| e == 5}
-even_numbers = list.filter {|e| e.even?}
+all_true_numbers = list.all? { |e| e < 5 }
+all_false_numbers = list.all? { |e| e > 5 }
+any_true_two = list.any? { |e| e == 2 }
+any_false_five = list.any? { |e| e == 5 }
+even_numbers = list.filter(&:even?)
 
 # Output Tests
 
