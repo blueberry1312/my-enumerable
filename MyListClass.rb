a74cb3c6e67ede41bc3bcb031f6c916349524f23
gdumani <gdumani@gmail.com>
2023-05-11 15:31:25 -0600

    assign MyList to a file and crea MyEnumerables module

---
+++ MyListClass.rb
@@ -0,0 +1,17 @@
+require './MyEnumerable'
+
+class MyList
+  extend MyEnumerable
+
+  def initialize(*arg)
+    @list = arg
+  end
+
+  def each
+    return to_enum unless block_given?
+
+    for x in @list do
+      yield(x)
+    end
+  end
+end
